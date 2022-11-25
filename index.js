e4abaf1019375343e07cf9e4d309ee4a9f4b0606
Evelin N <107203586+dev-evelin@users.noreply.github.com>
2022-11-25 14:02:28 +0000

    Files upload

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+
+
+
+setTimeout(function () {
+    modal.style.display = "block";
+  }, 1500);
+    
+  modalCloseBtn.addEventListener("click", function () {
+    modal.style.display = "none";    
+  });
+  
